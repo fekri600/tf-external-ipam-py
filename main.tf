c7c724e1504a776d99c57a88a8ceae441bdb3c41
Fekri Saleh <fekri600@gmail.com>
2025-05-15 11:13:14 -0600

    approved setup with rds role

--- main.tf
+++ main.tf
@@ -38,59 +38,61 @@ module "staging" {
   ]
 }
 
-# module "production" {
-#   source      = "./modules/environment"
-#   environment = "production"
-#   prefix      = local.name_prefix
+module "production" {
+  source      = "./modules/environment"
+  environment = "production"
+  project_settings = var.project_settings
+  prefix           = local.name_prefix
 
-#   policies_path = local.policies
-#   scripts_path  = local.scripts
+  policies_path   = local.policies
+  scripts_path    = local.scripts
+  security_groups = var.security_groups
 
-#   launch_template       = var.launch_template.production
-#   ec2_security_group_id = module.network.ec2_security_group_id
-#   autoscaling           = var.autoscaling.production
-#   target_group_arn      = module.network.target_group_arn
+  launch_template       = var.launch_template.production
+  ec2_security_group_id = module.network.ec2_security_group_id
+  autoscaling           = var.autoscaling.production
+  target_group_arn      = module.network.target_group_arn
 
-#   database              = var.database.production
-#   private_subnet_ids    = module.network.private_subnet_ids
-#   db_security_group_ids = [module.network.db_security_group_id]
+  database              = var.database.production
+  private_subnet_ids    = module.network.private_subnet_ids
+  db_security_group_ids = [module.network.db_security_group_id]
 
-#   rds_subnet_group_name = module.network.rds_subnet_group_name
+  rds_subnet_group_name = module.network.rds_subnet_group_name
 
-#   redis                   = var.redis.production
-#   redis_subnet_group_name = module.network.redis_subnet_group_name
-#   redis_security_group_id = module.network.redis_security_group_id
+  redis                   = var.redis.production
+  redis_subnet_group_name = module.network.redis_subnet_group_name
+  redis_security_group_id = module.network.redis_security_group_id
 
-#   depends_on = [
-#     module.network,
-#   ]
-# }
+  depends_on = [
+    module.network,
+  ]
+}
 
 
-# module "cloudwatch" {
-#   source     = "./modules/cloudwatch"
-#   aws_region = var.project_settings.aws_region
-#   alarm      = var.alarm
-#   logs       = var.logs
-#   env_configs = {
-#     staging = {
-#       asg_name = module.staging.asg_name
-#       rds_id   = module.staging.rds_id
-#       redis_id = module.staging.redis_id
-#     }
-#     production = {
-#       asg_name = module.production.asg_name
-#       rds_id   = module.production.rds_id
-#       redis_id = module.production.redis_id
-#     }
-#   }
-#   vpc_id = module.network.vpc_id
+module "cloudwatch" {
+  source     = "./modules/cloudwatch"
+  aws_region = var.project_settings.aws_region
+  alarm      = var.alarm
+  logs       = var.logs
+  env_configs = {
+    staging = {
+      asg_name = module.staging.asg_name
+      rds_id   = module.staging.rds_id
+      redis_id = module.staging.redis_id
+    }
+    production = {
+      asg_name = module.production.asg_name
+      rds_id   = module.production.rds_id
+      redis_id = module.production.redis_id
+    }
+  }
+  vpc_id = module.network.vpc_id
 
-#   depends_on = [
-#     module.network,
-#     module.staging,
-#     module.production,
-#   ]
-# }
+  depends_on = [
+    module.network,
+    module.staging,
+    module.production,
+  ]
+}
 
 
